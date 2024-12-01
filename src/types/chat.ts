b2647a7684aafc8e27471c0683d0b74019dcac07
renj01 <m4rknatad@gmail.com>
2024-12-01 16:17:57 +0800

    Update chat.ts

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -68,6 +68,7 @@ export type ModelOptions =
   | 'gemma2-9b-it'
   | 'mixtral-8x7b-32768'
   | 'gpt-4o-mini-northflank'
+  | 'meta-llama/llama-3.1-70b-instruct:free'
 // | 'gpt-3.5-turbo-0301'
 // | 'gpt-4-0314'
 // | 'gpt-4-32k-0314'
